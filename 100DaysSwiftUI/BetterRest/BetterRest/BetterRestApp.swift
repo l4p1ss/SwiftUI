d8d8dc0356e463e1d688363974c087148bb02d48
lollopesci <lorenzo.pesci1@stud.unifi.it>
2022-07-15 18:21:47 +0200

    added BetterRest App + challenges completed

---
+++ 100DaysSwiftUI/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Lorenzo Pesci on 14/07/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
